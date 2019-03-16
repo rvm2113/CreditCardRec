e311370c991d1d3eb49e85484df799e935d508b6
rvm2113 <manohr@rpi.edu>
2019-03-16 14:36:32 -0400

    Added two components

--- CreditApp/src/app/app.component.ts
+++ CreditApp/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams, HttpRequest} from "@angular/common/http";
+import {MatSelectModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import * as _ from 'lodash';
+// import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  	<main-information> </main-information>
+  	<credit-card-type> </credit-card-type>
+
+
+
+  `
 })
 export class AppComponent {
   title = 'CreditApp';
--- CreditApp/src/app/app.module.ts
+++ CreditApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainInformationComponent } from './main-information/main-information.component';
+import { CreditCardTypeComponent } from './credit-card-type/credit-card-type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainInformationComponent,
+    CreditCardTypeComponent
   ],
   imports: [
     BrowserModule,
