bb75e16eb6d7337291ce169efdb4598b04fc4aa3
rvm2113 <manohr@rpi.edu>
2019-03-16 22:51:44 -0400

    New Component

---
+++ CreditApp/src/app/rec/rec.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rec',
+  templateUrl: './rec.component.html',
+  styleUrls: ['./rec.component.css']
+})
+export class RecComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
